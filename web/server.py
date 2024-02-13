fec339d2960b2d9280d31344b5cdbe3636149a6a
Johnny <jmnaze123@gmail.com>
2024-02-13 16:58:00 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,21 +1,25 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__, template_folder="./template", static_folder="./static")
 port = 8090
 
+CORRECT_ANSWER = "apple"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/<path>")
-def page(path):
-    if not os.path.exists("templates/" + path + ".html"):
-        return render_template("404.html"), 404
-
-    return render_template(path + ".html")
+@app.route("/guess/<guess>")
+def guess(guess):
+    if guess == CORRECT_ANSWER:
+        return "CORRECT"
+    else:
+        return "WRONG"
 
 
 if __name__ == "__main__":
