77ac1dddce8e44c936fa374da49ce987300b5546
iarivera <iar3@njit.edu>
2024-02-20 21:06:53 -0500

    fixed OSError, and showing general order of pixelation

--- web/imagePixelation.py
+++ web/imagePixelation.py
@@ -6,17 +6,18 @@ def pixelate(image_path, resize, grayscale):
     
     #open image
     im = Image.open(image_path)
+    #resize image
     small_im = im.resize((resize,resize))
     new_im = small_im.resize(im.size, Image.NEAREST)
     #check if grayscale is wanted
     if (grayscale == 1):
             new_im = new_im.convert("1")
-    #new dimensions via list comprehension
-    #function could call in dimensions to resize image
     #showing image to confirm
     new_im.show()
-    
-pixelate('static\imgs\acm-logo.jpg', 10, 0)
-pixelate('static\imgs\acm-logo.jpg', 50, 1)
-pixelate('static\imgs\acm-logo.jpg', 100, 0)
+
+#currently getting an OSError calling these functions
+#fixed by swapping what slashes I used  
+pixelate('static/imgs/acm-logo.jpg', 10, 1)
+pixelate('static/imgs/acm-logo.jpg', 50, 0)
+pixelate('static/imgs/acm-logo.jpg', 100, 0)
 
