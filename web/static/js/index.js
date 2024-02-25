266da98bcc5608864c400804734cf4b99d35241a
Trang Dang <trangttdang01@gmail.com>
2024-02-25 18:30:18 -0500

    update more hints (less blurry)

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -9,8 +9,13 @@ var object_name = "apple"
 const storedObject = localStorage.getItem(object_name);
 if (storedObject) {
   log('Object found in localStorage:', storedObject);
-  globalThis.the_object = JSON.parse(storedObject);
-  initGame();
+  // globalThis.the_object = JSON.parse(storedObject);
+  fetch("static/json/apple.json")
+    .then((response) => response.json())
+    .then((data) => {
+      globalThis.the_object = data;
+      initGame();
+    });
 } else {
   log('Object NOT found in localStorage, fetching from server...');
   // If not, fetch the image URL
@@ -39,8 +44,8 @@ function initGame() {
   globalThis.startTime = startTime;
   globalThis.hintsContainer = hintsContainer;
   globalThis.img = img;
-  
-  img.src = the_object.image_url;
+
+  img.src = storedObject ? `static/imgs/${the_object.imgs[1]}` : the_object.image_url;
   img.style.width = "300px";
 
   document.getElementById("submit").addEventListener("click", function () {
@@ -67,26 +72,26 @@ function send_guess(guess) {
     });
 }
 
-function add_hint(hint){
-    let display =``;
-    switch (hint){
-        case "firstLetter":
-            display = `<h3>The first letter is ${the_object.name[0]}</h3>`;
-            break;
-        case "category":
-            display = `<p>The category is ${the_object.category}</p>`;
-            break;
-        default:
-            break;
-    }
-
-    let card = document.createElement("div");
-    card.innerHTML = display;
-
-    card.classList.add("hint-card");
-
-    hintsContainer.appendChild(card);
+function add_hint(hint) {
+  let display = ``;
+  switch (hint) {
+    case "firstLetter":
+      display = `<h3>The first letter is ${the_object.name[0]}</h3>`;
+      break;
+    case "category":
+      display = `<p>The category is ${the_object.category}</p>`;
+      break;
+    default:
+      break;
   }
+
+  let card = document.createElement("div");
+  card.innerHTML = display;
+
+  card.classList.add("hint-card");
+
+  hintsContainer.appendChild(card);
+}
 
 function check_guess(guess) {
   if (guess === "CORRECT") {
@@ -101,24 +106,27 @@ function check_guess(guess) {
     console.log(wrong_guesses);
 
     if (wrong_guesses === 2) {
-      // add color to image
-      console.log("Adding color to image");
+      // make image less blurry
+      img.src = `static/imgs/${the_object.imgs[2]}`;
+      console.log("Making image less blurry");
     }
 
     if (wrong_guesses === 3) {
+      // add color to image
+      img.src = `static/imgs/${the_object.imgs[3]}`;
+      console.log("Adding color to image");
+    }
+
+    if (wrong_guesses === 4) {
       // give category hint
       add_hint("category");
       console.log(`Hint: ${the_object.category}`);
     }
 
-    if (wrong_guesses === 4) {
+    if (wrong_guesses === 5) {
       // make image less blurry and give first letter hint
       add_hint("firstLetter");
-      console.log("Making image less blurry");
-    }
-
-    if (wrong_guesses === 5) {
-      // make image less blurry
+      img.src = `static/imgs/${the_object.imgs[4]}`;
       console.log("Making image less blurry");
     }
 
