c2868c19a39b4df719037ecfe0ce7847b926805c
Johnny <jmnaze123@gmail.com>
2024-03-05 16:46:31 -0500

    Update index.js
    Added comments

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -5,14 +5,18 @@ var wrong_guesses = 0; // the number of wrong guesses the user has made
 
 function initGame() {
   const startTime = new Date().getTime();
+
+  // get the elements from the DOM
   const hintsContainer = document.getElementById("hints-container");
   const img = document.getElementById("image");
 
+  // set the global variables
   globalThis.startTime = startTime;
   globalThis.hintsContainer = hintsContainer;
   globalThis.img = img;
 
   document.getElementById("submit").addEventListener("click", function () {
+    // only send the guess if the user has entered something
     if (document.getElementById("name").value !== "") {
       guess = document.getElementById("name").value;
       send_guess(guess);
@@ -21,6 +25,8 @@ function initGame() {
 }
 
 function send_guess(guess) {
+  // send the guess to the server
+  // the response will either be "CORRECT" or "WRONG"
   fetch("/guess/" + guess)
     .then((response) => response.text())
     .then((data) => {
@@ -32,14 +38,23 @@ function send_guess(guess) {
 function check_guess(response) {
   if (response === "CORRECT") {
     const endTime = new Date().getTime();
-    document.getElementById("submit").disabled = true; // stop the player from guessing
+
+    // stop the player from guessing
+    document.getElementById("submit").disabled = true;
+
+    // calculate time taken to get the correct answer
     const time = (endTime - startTime) / 1000;
+
+    // make image the original image
     img.src = `static/imgs/${the_object.imgs[0]}`;
+
     log(`You guessed correctly in ${time} seconds`);
   } else {
-    
+    // the guess was wrong
     wrong_guesses++;
     log("Wrong guess " + wrong_guesses);
+
+    // if the player makes 1 wrong guess, don't do anything
 
     if (wrong_guesses === 2) {
       // make image less blurry
@@ -64,16 +79,16 @@ function check_guess(response) {
       log("Making image less blurry");
     }
 
+    // if the player makes 6 wrong guesses, they fail
     if (wrong_guesses === 6) {
-      // fail?
       log("You failed");
       document.getElementById("submit").disabled = true; // stop the player from guessing
     }
   }
 }
 
-// get the object from the json
-// path: ../json/object.json
+// get the object from the file and run the game
+// need to get the name of the json from the server
 fetch("static/json/apple.json")
   .then((response) => response.json())
   .then((data) => {
