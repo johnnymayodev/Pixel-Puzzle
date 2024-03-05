b2da5564d27e7533829ad3069b5aafe39bdebf46
Johnny <jmnaze123@gmail.com>
2024-03-05 16:29:31 -0500

    Update server.py
    Added multiprocess support for downloading new images and a specific time when the server is running

--- web/server.py
+++ web/server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, jsonify
-
 import os
 import sys
+import time
+import multiprocessing
+from flask import Flask, render_template
 
+# add the backend module to the path
 script_path = os.path.dirname(__file__)
 module_path = os.path.join(script_path, "..", "backend")
 sys.path.append(module_path)
@@ -12,11 +14,14 @@ import api
 app = Flask(__name__, template_folder="./template", static_folder="./static")
 port = 8090
 
-CORRECT_ANSWER = "apple"
+NEW_OBJECT_TIME = "4:00"  # a new object is picked out 4:00 every day
+DEBUG = True
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 @app.route("/")
 def index():
@@ -25,18 +30,26 @@ def index():
 
 @app.route("/guess/<guess>")
 def guess(guess):
-    if guess == CORRECT_ANSWER:
+    if guess.lower() == CORRECT_ANSWER:
         return "CORRECT"
     else:
         return "WRONG"
 
-@app.route('/get_image_url/<object_name>', methods=['GET'])
-def get_image_url(object_name):
-    img_url = api.make_api_call(object_name)
-    if img_url:
-        return jsonify({'image_url': img_url})
-    else:
-        return jsonify({'error': 'Failed to retrieve image URL'}), 500
+
+def print_time():
+    time.sleep(3)  # wait for the server to start
+    while True:
+        if time.strftime("%H:%M") == NEW_OBJECT_TIME:
+            # pick out a new object
+            api.download_images(CORRECT_ANSWER)
+        time.sleep(59)  # sleep for 59 seconds
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=port)
+    CORRECT_ANSWER = "apple"  # this should be replaced when we implement picking out a new object every 24 hours
+    api.download_images(CORRECT_ANSWER)
+
+    if not DEBUG:
+        clock = multiprocessing.Process(target=print_time)
+        clock.start()
+    app.run(port=port, debug=DEBUG)
