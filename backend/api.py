659e084b8e866e017cc32ee0715f20d6e508c33c
Johnny <jmnaze123@gmail.com>
2024-04-02 14:44:30 -0400

    Update api.py
    added functions for downloading images for timed mode

--- backend/api.py
+++ backend/api.py
@@ -4,6 +4,12 @@ from PIL import Image, ImageFilter
 from dotenv import load_dotenv
 
 load_dotenv()
+PEXEL_API_KEY = os.getenv("PEXEL_API_KEY")
+THESAURUS_API_KEY = os.getenv("THESAURUS_API_KEY")
+
+URL = "https://api.pexels.com/v1/search"
+HEADERS = {"Authorization": PEXEL_API_KEY}
+PARAMETERS = {"per_page": 1}
 
 # Variables for the image maker
 NUMBER_OF_IMAGES = 4
@@ -14,15 +20,8 @@ FILE_NAMES = ["obj_1", "obj_2", "obj_3", "obj_4"]
 
 
 def download_images(obj):
-    # get the API key for Pexels
-    PEXEL_API_KEY = os.getenv("PEXEL_API_KEY")
-
-    # Variables for the API request
-    HEADERS = {"Authorization": PEXEL_API_KEY}
-    PARAMETERS = {"per_page": 1}
     print("Downloading images for", obj)
 
-    url = "https://api.pexels.com/v1/search"
     PARAMETERS["query"] = obj
 
     # remove old images
@@ -33,7 +32,7 @@ def download_images(obj):
 
     try:
         # make API request
-        response = requests.get(url, headers=HEADERS, params=PARAMETERS)
+        response = requests.get(URL, headers=HEADERS, params=PARAMETERS)
         response.raise_for_status()
 
         # get the image URL
@@ -72,7 +71,6 @@ def image_maker(obj):
 
 
 def get_synonyms(obj):
-    THESAURUS_API_KEY = os.getenv("THESAURUS_API_KEY")
     url = "https://api.api-ninjas.com/v1/thesaurus?word={}".format(obj)
     response = requests.get(url, headers={"X-Api-Key": THESAURUS_API_KEY})
     if response.status_code == requests.codes.ok:
@@ -81,3 +79,47 @@ def get_synonyms(obj):
     else:
         print("Error:", response.status_code, response.text)
         return None
+
+def download_images_timed_mode(objs):
+    
+    if os.path.exists("web/static/imgs/timed/"):
+        os.system("rm web/static/imgs/timed/*.jpg")
+        
+    for i, obj in enumerate(objs):
+        try:
+            PARAMETERS["query"] = obj
+            
+            # make API request
+            response = requests.get(URL, headers=HEADERS, params=PARAMETERS)
+            response.raise_for_status()
+
+            # get the image URL
+            data = response.json()
+            img_url = data["photos"][0]["src"]["original"]
+            response = requests.get(img_url)
+        
+            # check if imgs folder exists and create it if it doesn't
+            if not os.path.exists("web/static/imgs/timed"):
+                os.makedirs("web/static/imgs/timed")
+                
+            # save the image
+            with open(f"web/static/imgs/timed/{i}.jpg", "wb") as file:
+                file.write(response.content)
+                
+        except Exception as e:
+            print("Error making API request:", e)
+            return None
+        
+    image_maker_timed_mode(objs)
+        
+def image_maker_timed_mode(objs):
+    for i, obj in enumerate(objs):
+        for j in range(NUMBER_OF_IMAGES):
+            try:
+                im = Image.open(f"web/static/imgs/timed/{i}.jpg")  # get the original image
+                im = im.filter(ImageFilter.GaussianBlur(BLUR_LEVELS[j]))  # blur the image
+                if GRAYS[j]:  # if GRAYS[j] == 1 (if we want grayscale)
+                    im = im.convert("L")  # convert to grayscale
+                im.save(f"web/static/imgs/timed/{i}_{j}.jpg")  # save the image
+            except Exception as e:
+                print("An error occurred:", e)
